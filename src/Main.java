1efc0f2d447825c618e6c912c794ef10f233f9a2
YourName <your@email.com>
2026-03-24 19:23:15 +0530

    UC2: Added Room Types & Static Availability

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,63 @@
-/**
- * Hotel Booking Application Entry Point
- * This class starts the application.
- * @author Yuvasri
- * @version 1.0
- */
+abstract class Room {
+    String roomType;
+    int beds;
+    double price;
 
-public class Main {
+    Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Hotel Booking System");
-        System.out.println("Version: 1.0");
-
+    void displayInfo() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+        System.out.println("------------------------");
     }
 }
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1500);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2500);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 4500);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("🏨 Hotel Room Types & Availability (UC2)");
+
+        // Static availability
+        int availableSingle = 5;
+        int availableDouble = 3;
+        int availableSuite = 2;
+
+        // Create room objects
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        // Display Info
+        r1.displayInfo();
+        System.out.println("Availability: " + availableSingle);
+
+        r2.displayInfo();
+        System.out.println("Availability: " + availableDouble);
+
+        r3.displayInfo();
+        System.out.println("Availability: " + availableSuite);
+    }
+}/
